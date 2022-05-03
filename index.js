fe5fb08944fbb1d050d01c670045a1be4d944021
Nikhil Jose <nikhiljose12345@gmail.com>
2022-05-03 17:04:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function showImages(){
+    document.getElementById("content").innerHTML=""
+    let url=document.getElementById("urlbox").value
+    axios.get(`https://zip-img-extractor.herokuapp.com/api?url=${url}`).then(res=>{
+        console.log(res.data)
+        res.data.forEach(img => {
+            document.getElementById("content").innerHTML+=`<img src="${img}"> <br>`
+        });
+    })
+    .catch(err=>{
+        console.log(err)
+        document.getElementById("content").innerHTML=`<h4>Some Error Occures</h4>${err.response.data}`
+    })
+}
+
+function clearData(){
+    console.log("trigger")
+    document.getElementById("urlbox").value=""
+    document.getElementById("content").innerHTML=""
+}
